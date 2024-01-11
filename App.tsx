bdb24595bf01fc346b67afbd8e18ae80fd6c8bb7
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-11 20:09:16 +0530

    classes and its need

--- App.tsx
+++ App.tsx
@@ -7,31 +7,26 @@ import { Text,Button,StyleSheet } from 'react-native';
 
 function App(): React.JSX.Element {
 
-  var cal={
-number1:0,
-number2:0,
-addition:function(){
 
-  console.log(this.number1+this.number2)
-},
-
-subtraction:function(){
-  console.log(this.number1-this.number2)
+  class Calculator{
+number1:number=0;
+number2:number=0;
+addition(){
+  console.log(this.number1+this.number2);
 }
   }
 
-  function AddNumber(){
-cal.number1=30;
-cal.number2=40;
-cal.addition();
 
-  }
+  var cla1=new Calculator();
+cla1.number1=30;
+cla1.number2=40;
+cla1.addition();
 
-  function SubNumber(){
-    cal.number1=90;
-    cal.number2=60;
-    cal.subtraction();
-  }
+var cal2=new Calculator();
+cal2.number1=40;
+cal2.number2=20;
+cal2.addition(); 
+  
 
 return (
   <View style={{alignItems:'center' ,
@@ -39,8 +34,7 @@ return (
   flex:1,
   justifyContent:'center' }}>
   
-<Button title='Addition' onPress={AddNumber}/>
-<Button title='Subtraction' onPress={SubNumber}/>
+
   </View>
   );
 }
