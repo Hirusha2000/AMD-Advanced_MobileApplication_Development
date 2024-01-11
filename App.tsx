293b309be9902f463183aa9aea234c3c422fd3c4
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-11 11:06:17 +0530

    textFields

--- App.tsx
+++ App.tsx
@@ -1,46 +1,52 @@
 
 import React from 'react';
-import { View } from 'react-native';
+import { TextInput, View } from 'react-native';
 import { Text,Button } from 'react-native';
 import CustomLabel from './src/components/CustomLabel';
 
 
 function App(): React.JSX.Element {
 
-  console.log('Hello')
-
-  function Click(x:number,y:number){
-    console.log("Addition is "+(x+y))
-    console.log("Multiplication is "+(x*y))
-  }
-
-  
- 
 
 
   return (
   <View style={{alignItems:'center'}}>
+   <TextInput style={{
+    backgroundColor:'green',
+    color:'black',
+    width:300,
+     marginTop:50,
+     borderRadius:40,
+     paddingLeft:20}}
+
+
+     placeholder='User Name'
+     placeholderTextColor={'white'}
+     
+     />
+
+<TextInput style={{
+    backgroundColor:'green',
+    color:'black',
+    width:300,
+     marginTop:50,
+     borderRadius:40,
+     paddingLeft:20}}
+
+     secureTextEntry={true}
+     placeholder='Password'
+     placeholderTextColor={'white'}
+     
+     
+     />
  
- <Button title='Click Me' onPress={()=>{
-    Click(10,5)
- }}/>
   </View>
   );
 }
 
 
-type customTextProps={
-  fsize?:number
-  a:string
-  b:string
-  c?:string
 
-}
 
-function CustomText(props:customTextProps){
-  return(
-    <Text style={{ fontSize:props.fsize}}> {props.a} {props.b} {props.c}</Text>
-  );
-}
+
 
 export default App;
