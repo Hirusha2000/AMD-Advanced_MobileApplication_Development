349eaaf7dc96ed743c28cfdcb4b5c1597981e722
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-11 10:50:25 +0530

    addition and mutiplication

--- App.tsx
+++ App.tsx
@@ -5,23 +5,41 @@ import { Text,Button } from 'react-native';
 import CustomLabel from './src/components/CustomLabel';
 
 
-
-
 function App(): React.JSX.Element {
+
+  console.log('Hello')
+
+  function Click(x:number,y:number){
+    console.log("Addition is "+(x+y))
+    console.log("Multiplication is "+(x*y))
+  }
+
+  
  
+
+
   return (
   <View style={{alignItems:'center'}}>
-   <CustomText  fSize={60} a={'Hello'} b={'world'}/>
-    <CustomLabel>Welcome</CustomLabel>
  
+ <Button title='Click Me' onPress={()=>{
+    Click(10,5)
+ }}/>
   </View>
   );
 }
 
 
-function CustomText(props:any){
+type customTextProps={
+  fsize?:number
+  a:string
+  b:string
+  c?:string
+
+}
+
+function CustomText(props:customTextProps){
   return(
-    <Text style={{ fontSize:props.fSize}}> {props.a} {props.b} </Text>
+    <Text style={{ fontSize:props.fsize}}> {props.a} {props.b} {props.c}</Text>
   );
 }
 
