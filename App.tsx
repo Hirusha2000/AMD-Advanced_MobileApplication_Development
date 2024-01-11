259910def49b65a236238f8f7b3df14a1b13cd02
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-11 12:35:58 +0530

    react state management

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {useState}from 'react';
 import { TextInput, View } from 'react-native';
 import { Text,Button } from 'react-native';
 import CustomLabel from './src/components/CustomLabel';
@@ -7,42 +7,23 @@ import CustomLabel from './src/components/CustomLabel';
 
 function App(): React.JSX.Element {
 
+  console.log("App Component is Rendered.");
 
+  const [count,setCount]=useState(0);
+
+  function changeCount(){
+    setCount(count+1);
+
+  }
 
   return (
   <View style={{alignItems:'center'}}>
-   <TextInput style={{
-    backgroundColor:'green',
-    color:'black',
-    width:300,
-     marginTop:50,
-     borderRadius:40,
-     paddingLeft:20}}
-
-
-     placeholder='User Name'
-     placeholderTextColor={'white'}
-     
-     />
-
-<TextInput style={{
-    backgroundColor:'green',
-    color:'black',
-    width:300,
-     marginTop:50,
-     borderRadius:40,
-     paddingLeft:20}}
-
-     secureTextEntry={true}
-     placeholder='Password'
-     placeholderTextColor={'white'}
-     
-     
-     />
- 
+<Text style={{fontSize:30}}>Count is :{count}</Text>
+<Button title='Change Count' onPress={changeCount}/>
   </View>
   );
 }
+
 
 
 
