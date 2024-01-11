48bd2b79285213630129275e7316bb69041a8679
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-11 21:01:51 +0530

    flex

--- App.tsx
+++ App.tsx
@@ -8,37 +8,31 @@ import { Text,Button,StyleSheet } from 'react-native';
 function App(): React.JSX.Element {
 
 
-  interface Student{
-name:string;
-age:number;
-university:string;
-  }
-
-  var student_1:Student={
-    name:'Hirusha',
-    age:24,
-    university:'IJSE',
-    
-  }
-
-  var text='abc';
-
-function printData(std:Student){
-console.log(std.name);
-console.log(std.age);
-console.log(std.university);
-}
 
 
 
 return (
-  <View style={{alignItems:'center' ,
-  backgroundColor:'white',
-  flex:1,
-  justifyContent:'center' }}>
-  
-  <Button title='print' onPress={ ()=>printData(student_1)}/>
-  </View>
+ 
+<View style={{backgroundColor:'yellow',flex:1, flexDirection:'row'}}>
+<View style={{backgroundColor:'green',flex:1}}>
+
+
+</View>
+<View style={{backgroundColor:'yellow',flex:4}}>
+
+
+</View>
+
+<View style={{backgroundColor:'red',flex:2}}>
+
+
+</View>
+
+
+
+</View>
+
+
   );
 }
 
