3d9a689fc9c357051ae784c6cd8205e22aaa4724
Hirusha2000 <2000hirushaudara@gmail.com>
2024-01-11 09:46:50 +0530

    learning custom components

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Text,Button } from 'react-native';
+import MyComponents from './src/components/MyComponents';
 
 
 
@@ -9,24 +10,7 @@ function App(): React.JSX.Element {
  
   return (
   <View style={{alignItems:'center'}}>
-      <Text style={{fontSize:40,
-      color:'green',
-    marginLeft:10,
-  marginTop:10}}>Hello Hirusha</Text>
-  <Text style={
-    {
-      fontSize:30,
-      color:'red',
-      marginTop:10,
-      marginBottom:10
-    }
-  }>
-    
-    React Native
-
-
-  </Text>
-  <Button title='ClickMe'/>
+    <MyComponents/>
   </View>
   );
 }
